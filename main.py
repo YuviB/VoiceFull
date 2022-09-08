bb5dc4c4e935ffd428951c6a07080f4a3efe49e3
Yuvi <stu21010@ashs.school.nz>
2022-09-08 17:48:31 +1200

    Added login check system, add same file directory and write fucntion to log users status vis a vi login or not.

--- main.py
+++ main.py
@@ -4,55 +4,93 @@ import pywhatkit
 import wikipedia
 import datetime
 
+#global user_check
 
-listener = sr.Recognizer()
-engine = pyttsx3.init()
-voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+#f.write(r"C:\PythonData\usercheck.txt", "a")
 
+with open(r"C:\PythonData\usercheck.txt", "w") as f:
+    f.write(" ")
 
-def speak(text):
-    engine.say(text)
-    engine.runAndWait()
+#read
 
+with open(r"C:\PythonData\usercheck.txt", "r") as f:
+   print("New text:\n",f.read())
 
-def take_command():
-    try:
-        with sr.Microphone() as source:
-            print('listening...')
-            voice = listener.listen(source)
-            command = listener.recognize_google(voice)
-            command = command.lower()
-            if 'atlas' in command:
-                command = command.replace('atlas', '')
-                print(command)
-    except:
-        pass
-    return command
+#append
+
+with open(r"C:\PythonData\usercheck.txt", "a") as f:
+    f.write("\n This text was added using Append.")
 
 
-def run_atlas():
-    command = take_command()
-    print(command)
-    if 'play' in command:
-        song = command.replace('play', '')
-        speak('playing ' + song)
-        pywhatkit.playonyt(song)
+#read the appended text
 
-    elif 'time' in command:
-        time = datetime.datetime.now().strftime('%I:%M %p')
-        speak('The time is ' + time)
+with open(r"C:\PythonData\usercheck.txt", "r") as f:
+    print("Append:\n",f.read())
+
+with open(r"C:\PythonData\usercheck.txt", "r") as f:
+    pull_user_check = open(r"C:\PythonData\usercheck.txt")
 
 
-    elif 'Workspaces' and 'Open' in command:
-        speak('Sure thing, what workspace shall I open. ')
-
-    elif 'exit' in command:
-        exit()
-
-    else:
-        speak('Please say the command again.')
+print(pull_user_check)
+if "User Signed in = 1" in pull_user_check:
 
 
-while True:
-    run_atlas()
+
+
+
+    listener = sr.Recognizer()
+    engine = pyttsx3.init()
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+
+
+    def speak(text):
+        engine.say(text)
+        engine.runAndWait()
+
+
+    def take_command():
+        try:
+            with sr.Microphone() as source:
+                print('listening...')
+                voice = listener.listen(source)
+                command = listener.recognize_google(voice)
+                command = command.lower()
+                if 'atlas' in command:
+                    command = command.replace('atlas', '')
+                    print(command)
+        except:
+            pass
+        return command
+
+
+    def run_atlas():
+        command = take_command()
+        print(command)
+        if 'play' in command:
+            song = command.replace('play', '')
+            speak('playing ' + song)
+            pywhatkit.playonyt(song)
+
+        elif 'time' in command:
+            time = datetime.datetime.now().strftime('%I:%M %p')
+            speak('The time is ' + time)
+
+
+        elif 'Workspaces' and 'Open' in command:
+            speak('Sure thing, what workspace shall I open. ')
+
+        elif 'exit' in command:
+            exit()
+
+        else:
+            speak('Please say the command again.')
+
+
+    while True:
+        run_atlas()
+
+
+
+else:
+    print("Hmm it seems you arent signed in, open the sign in file to gain acssess")
