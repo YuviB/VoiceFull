a5f98d432b694afb8b5e78a51eaebb1f1fb92507
Yuvi <stu21010@ashs.school.nz>
2022-09-08 17:01:05 +1200

    Updated Voice Commands with Features & Functions added

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+
 
 
 listener = sr.Recognizer()
-try:
-    with sr.Microphone() as src:
-        print("Ready For Command")
-        voice = listener.listen(src)
-        command = listener.recognize_google(voice)
-        print(command)
-finally:
-    pass
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'atlas' in command:
+                command = command.replace('atlas', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_atlas():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        speak('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'hello' or 'hey'in command:
+        speak("Hello, How can I help")
+
+    else:
+        speak('Please say the command again.')
+
+
+while True:
+    run_atlas()
